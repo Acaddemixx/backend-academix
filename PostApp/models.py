2b6e8d9bf73bdf1207659c8a50b5b33095f4a09f
Danielyilma <yilmad752@gmail.com>
2024-04-02 04:01:03 +0300

    adding PostApp Models

--- PostApp/models.py
+++ PostApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('UserApp.Student', on_delete=models.SET_NULL)
+    club = models.ForeignKey('CommunityApp.Club', null=True, on_delete=models.SET_NULL)
+    section = models.ForeignKey('CommunityApp.Section', null=True, on_delete=models.SET_NULL)
+    course = models.ForeignKey('CommunityApp.Section', null=True, on_delete=models.SET_NULL)
+    created_at = models.DateField(auto_now_add=True)
+
+class Comment(models.Model):
+    content = models.TextField()
+    user = models.ForeignKey('UserApp.Student', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
+class Like(models.Model):
+    user = models.ForeignKey('UserApp.Student', on_delete=models.SET_NULL)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
