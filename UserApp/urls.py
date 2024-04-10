a8d51ef32be637e967441a663f1d228e7d4e5b21
Danielyilma <yilmad752@gmail.com>
2024-04-10 14:18:04 +0300

    api to get all users

--- UserApp/urls.py
+++ UserApp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.signUp, name='student-sign-up'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('test/', views.test_token, name='test-token')
+    path('test/', views.test_token, name='test-token'),
+    path('users/', views.get_users, name='get_user')
 ]
