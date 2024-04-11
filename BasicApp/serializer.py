1349e0836230565264cb5890bcd25d648da02c5d
Danielyilma <yilmad752@gmail.com>
2024-04-11 18:53:30 +0300

    update

--- BasicApp/serializer.py
+++ BasicApp/serializer.py
@@ -5,14 +5,14 @@ from .models import Department, Course, Building
 class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Department
-        fields = ['head', 'name', 'overview']
+        fields = ["id", 'head', 'name', 'overview']
 
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
-        fields = ['name', 'deparment', 'academic_year', 'semester', 'credit_hr', 'lecture_hr', 'overview']
+        fields = ["id", 'name', 'deparment', 'academic_year', 'semester', 'credit_hr', 'lecture_hr', 'overview']
 
 class BuildingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Building
-        fields = ['name', 'block_number', 'tyoe', 'description']
+        fields = ["id", 'name', 'block_number', 'tyoe', 'description']
